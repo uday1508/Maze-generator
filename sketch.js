aa7f184cf32ce6c90b8a6a5be053bd2f5024d097
uday <121241786+uday1508@users.noreply.github.com>
2023-02-03 18:31:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,22 @@ function setup() {
   createCanvas(500, 500);
   rows = width/w;
   cols = height/w;
+  for(var i = 0 ; i < rows ; i++)
+  {
+    for(var j = 0 ; j < cols ; j++)
+    {
+      var cell = new Cell(j,i);
+      boxes.push(cell);
+    }
+  }
 }
 
 function draw() {
   background(220);
+}
+
+function Cell(i,j)
+{
+  this.i = i;
+  this.j = j;
 }
