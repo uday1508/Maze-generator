0cd0824dfca1bd45afe046da395c035f5f1712b7
uday <121241786+uday1508@users.noreply.github.com>
2023-02-04 19:59:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,10 +18,17 @@ function setup() {
 function draw() {
   background(220);
   for(var i = 0 ; i < boxes.length; i++) boxes[i].show();
+  
 }
 
 function Cell(i,j)
 {
   this.i = i;
   this.j = j;
+  this.show = function()
+  {
+    var x = this.i*w;
+    var y = this.j*w;
+    rect(x,y,w,w);
+  }
 }
